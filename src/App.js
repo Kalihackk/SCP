74a8ea99afc1ea9b9a36c5eee7a41bc0d7cd4111
Timur <sherillblossomforever@gmail.com>
2022-11-22 19:03:54 +0600

    asd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./regForm.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form class="form" autocomplete="off">
+        <div class="control">
+          <h1>Sign In</h1>
+        </div>
+        <div class="control block-cube block-input">
+          <input name="username" type="text" placeholder="Username" />
+          <div class="bg-top">
+            <div class="bg-inner"></div>
+          </div>
+          <div class="bg-right">
+            <div class="bg-inner"></div>
+          </div>
+          <div class="bg">
+            <div class="bg-inner"></div>
+          </div>
+        </div>
+        <div class="control block-cube block-input">
+          <input name="password" type="password" placeholder="Password" />
+          <div class="bg-top">
+            <div class="bg-inner"></div>
+          </div>
+          <div class="bg-right">
+            <div class="bg-inner"></div>
+          </div>
+          <div class="bg">
+            <div class="bg-inner"></div>
+          </div>
+        </div>
+        <button class="btn block-cube block-cube-hover" type="button">
+          <div class="bg-top">
+            <div class="bg-inner"></div>
+          </div>
+          <div class="bg-right">
+            <div class="bg-inner"></div>
+          </div>
+          <div class="bg">
+            <div class="bg-inner"></div>
+          </div>
+          <div class="text">Log In</div>
+        </button>
+      </form>
     </div>
   );
 }
